13b3dd00cf51070bb0979a847788ba6846be7610
MorriganCube <mfreemansheehy1@gmail.com>
2025-01-24 10:47:54 -0800

    implemented argparse, switched to local app

--- adminLogIn.py
+++ adminLogIn.py
@@ -3,17 +3,26 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
+import argparse
 import time
 
 
 
 
+
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-method', required=True) # valid types: sql, brute
+    parser.add_argument('-attempts', nargs='?', const=10000) # make depth attempts, only for admin_brute
+    args = parser.parse_args()
+    method = str(args.method)
+    attempts = int(args.attempts)
+    
     # Initialize the WebDriver
     driver = webdriver.Chrome()  # Use the browser of your choice (e.g., Firefox, Edge)
 
     # Open the webpage
-    driver.get("https://juice-shop.herokuapp.com/#/")
+    driver.get("http://localhost:3000/")
 
     # Wait for and dismiss the overlay
     try:
@@ -25,8 +34,12 @@ def main():
     except Exception as e:
         print(f"Could not dismiss the overlay: {e}")
 
-    # logInAdminSql(driver)
-    bruteForcePassword(driver)
+    if(method == "sql"):
+        logInAdminSql(driver)
+    elif(method == "brute"):
+        bruteForcePassword(driver)
+    else:
+        print("Usage: adminLogin.py -method=[sql, brute]")
     # Close the browser
     input("Press Enter to close the browser...")
     driver.quit()
